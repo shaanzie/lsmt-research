fadc73ca8c701ee21f4548c9cdecc54082712b6a
shaanzie <ishaanlagwankar@gmail.com>
2020-09-05 19:39:19 +0530

    Testing added

--- scripts/profile_db.sh
+++ scripts/profile_db.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
-bash benchmark_profiles/profile_cassandra.sh
+# bash benchmark_profiles/profile_cassandra.sh
 
 #bash benchmark_profiles/profile_hbase.sh
 
-bash benchmark_profiles/profile_mongo.sh
+# bash benchmark_profiles/profile_mongo.sh
 
 #bash benchmark_profiles/profile_rocks.sh
+
+pidstat -h -d -r -s -u -T ALL 1000 -e ~/ycsb/bin/ycsb run -s -P mongodb-async ~/ycsb/workloads/workloada > sample.pidstat
