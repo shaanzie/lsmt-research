94074d90be4c987d0befb2a28ab5f8c8bda575da
Tejvi M <tejvimstays@gmail.com>
2020-09-14 00:36:20 +0530

    files to run workloads on pebblesdb along with the readme for instructions

---
+++ pebblesdb/benchmarks/benchmark_workload.cpp
@@ -0,0 +1,38 @@
+#include "./../../scripts/benchmarking/run_workload.cpp"
+#include <cassert>
+#include "pebblesdb/db.h"
+
+int main(int argc, char const *argv[]){
+
+  leveldb::DB* db;
+  leveldb::Options options;
+
+  options.create_if_missing = false;
+  options.error_if_exists = false;
+
+  leveldb::Status status = leveldb::DB::Open(options, argv[2], &db);
+  assert(("[ERROR] Cannot create database, the database might not exist", status.ok()));
+
+  long int total_iterations = atoi(argv[1]);
+
+  double read_proportion = 0, write_proportion = 0, update_proportion = 0;
+
+
+  if(argv[3] == "custom"){
+    read_proportion = std::atof(argv[4]);
+    write_proportion = std::atof(argv[5]);
+    update_proportion = std::atof(argv[6]);
+  }
+  else{
+    set_proportions_by_workload_type(argv[3], read_proportion, write_proportion, update_proportion);
+  }
+
+  workload_properties properties(total_iterations, read_proportion, write_proportion, update_proportion);
+
+  Workload<leveldb::DB*, leveldb::ReadOptions, leveldb::WriteOptions> read_heavy_workload(db, leveldb::ReadOptions(), leveldb::WriteOptions(), &properties);
+  read_heavy_workload.run();
+
+  delete db;
+
+  return 0;
+}
---
+++ pebblesdb/benchmarks/readme.md
@@ -0,0 +1,18 @@
+# Running benchmarks
+
+```
+ g++ benchmark_workload.cpp -lpebblesdb -lsnappy -lpthread --std=c++17
+ ./a.out <num_records> <db_location> <"read_heavy"|"update_heavy"|"write_heavy"|"read_and_modify">
+```
+
+example (to run a 1000 queries on database1 - write heavy workload):
+```
+g++ benchmark_workload.cpp -lpebblesdb -lsnappy -lpthread --std=c++17
+./a.out 10000 database1 write_heavy
+```
+
+additionally, [WIP]
+
+```
+./a.out <num_records> <db_location> custom <read_proportion> <write_proportion> <update_proportion>
+```
